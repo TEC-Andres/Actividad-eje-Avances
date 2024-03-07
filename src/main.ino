0b3817e6f2ee0c287b460f01276a99af3c558871
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-03-07 15:25:32 -0600

    main.ino

---
+++ src/main.ino
@@ -0,0 +1,61 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "texturas.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte* charArrays[] = {nullptr, c_fuego_1, c_fuego_2, c_fuego_3, c_fuego_4, c_fuego_5};
+
+int frame = 1;
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.backlight();
+  Serial.begin(9600);
+}
+
+void loop() {
+  title_screen();
+  button_initial();
+
+  Serial.println("Frame " + String(frame));
+  delay(160); // 6.25 FPS
+  increase_frame(5);
+}
+
+void title_screen() {
+  lcd.setCursor(2, 0);
+  lcd.write(1);
+  lcd.print(" Duol-ino ");
+  lcd.write(1);
+  animation_type_1();
+}
+
+void button_initial() {
+  button_texture();
+  lcd.setCursor(7, 1);
+  lcd.write(2);
+  lcd.write(3);
+  lcd.write(4);
+}
+
+void button_texture() {
+  lcd.createChar(2, c_boton_inicial_1);
+  lcd.createChar(3, c_boton_inicial_2);
+  lcd.createChar(4, c_boton_inicial_3);
+}
+
+void increase_frame(int limit) {
+  frame++;
+  if (frame > limit) {
+    frame = 1;
+  }
+}
+
+void animation_type_1() {
+  for (int i = 1; i <= 5; i++) {
+    String imageName = "c_fuego_" + String(i);
+    lcd.createChar(1, charArrays[i]);
+    delay(160);
+  }
+}
