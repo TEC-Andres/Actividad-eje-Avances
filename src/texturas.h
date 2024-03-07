2626288e7f1e260f757e99872a129d966130fd93
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-03-07 15:26:35 -0600

    texturas.h

---
+++ src/texturas.h
@@ -0,0 +1,16 @@
+#ifndef TEXTURAS_H
+#define TEXTURAS_H
+
+#include <Arduino.h>
+
+extern byte c_fuego_1[8];
+extern byte c_fuego_2[8];
+extern byte c_fuego_3[8];
+extern byte c_fuego_4[8];
+extern byte c_fuego_5[8];
+
+extern byte c_boton_inicial_1[8];
+extern byte c_boton_inicial_2[8];
+extern byte c_boton_inicial_3[8];
+
+#endif
