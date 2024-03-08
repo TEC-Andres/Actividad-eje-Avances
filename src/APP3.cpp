1159f518a9761fb417b5e18eb7a6cddb85e678e7
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-03-07 23:22:17 -0600

    Update and rename texturas.cpp to APP3.cpp

--- src/APP3.cpp
+++ src/APP3.cpp
@@ -1,5 +1,9 @@
-#include "texturas.h"
+#include "APP3.h"
 
+/* Animaciones */
+byte* a_fuego[] = {nullptr, c_fuego_1, c_fuego_2, c_fuego_3, c_fuego_4, c_fuego_5};
+
+/* Caracteres especiales */
 byte c_fuego_1[8] = { B00010, B00010, B00110, B00110, B10111, B11101, B10011, B01110 };
 byte c_fuego_2[8] = { B01000, B01010, B00010, B00110, B10111, B11101, B10011, B01110 };
 byte c_fuego_3[8] = { B00100, B00100, B01100, B01110, B11011, B10111, B10001, B01110 };
