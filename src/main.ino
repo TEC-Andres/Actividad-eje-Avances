b2dd36017cb6e88bb60c169b69658fdd5492e20b
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-03-07 23:19:58 -0600

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,57 +1,46 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include "texturas.h"
+#include "APP0.h"
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-byte* a_fuego[] = {nullptr, c_fuego_1, c_fuego_2, c_fuego_3, c_fuego_4, c_fuego_5};
 
 int frame = 1;
 
 void setup() {
   lcd.begin(16, 2);
   lcd.backlight();
-  Serial.begin(9600);
 }
 
 void loop() {
   title_screen();
-  button_initial();
-
-  Serial.println("Frame " + String(frame));
   delay(160); // 6.25 FPS
-  animar(a_fuego, frame, 5);
 }
 
 void title_screen() {
-  lcd.setCursor(2, 0);
-  lcd.write(1);
-  lcd.print(" Duol-ino ");
-  lcd.write(1);
-  // animation_type_1 is now called in the loop function
+  crearCaracter(DUO_OS, 8);
+  lcd.setCursor(6,0);
+  for(int i=1; i<=4; i++){
+    lcd.write(i);
+  }
+  lcd.setCursor(6,1);
+  for(int i=5; i<=8; i++){
+    lcd.write(i);
+  }
 }
 
-void button_initial() {
-  button_texture();
-  lcd.setCursor(7, 1);
-  lcd.write(2);
-  lcd.write(3);
-  lcd.write(4);
+void crearCaracter(byte* CARACTER[], uint8_t LIMITE) {
+  for (int i = 1; i <= LIMITE; i++) {
+    lcd.createChar(i, CARACTER[i]);
+  }
 }
 
-void button_texture() {
-  lcd.createChar(2, c_boton_inicial_1);
-  lcd.createChar(3, c_boton_inicial_2);
-  lcd.createChar(4, c_boton_inicial_3);
-}
-
-void animar(byte* ANIMACION[], int &currentFrame, int LIMITE) {
+void animar(byte* ANIMACION[], int &currentFrame, uint8_t LIMITE, uint8_t MEMORIAVAL) {
   currentFrame++;
   if (currentFrame > LIMITE) {
     currentFrame = 1;
   }
 
   for (int i = 1; i <= LIMITE; i++) {
-    lcd.createChar(1, ANIMACION[currentFrame]);
+    lcd.createChar(MEMORIAVAL, ANIMACION[currentFrame]);
   }
 }
