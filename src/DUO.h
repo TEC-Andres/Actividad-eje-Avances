d7493fc53c631425f74c3efcf40db845748954fc
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-04-04 16:34:12 -0600

    Update DUO.h

--- src/DUO.h
+++ src/DUO.h
@@ -1,3 +1,19 @@
+/*
+#       Sesión 30: Actividad eje
+#       Andrés Rodríguez Cantú ─ Daniela Mendez Rivas
+#
+#       Copyright (C) Tecnologico de Monterrey
+#
+#       Archivo: DUO.h
+#
+#       Datos del programa (Sin contar la consola de debugging)
+#       Almacenamiento: 7198 (22%) / 32,256 bytes
+#       RAM (Globales): 612 (29%) / 2,048 bytes
+#
+#       Creado:                   04/04/2024
+#       Última Modificación:      04/04/2024
+*/
+
 #ifndef DUO_H
 #define DUO_H
 
