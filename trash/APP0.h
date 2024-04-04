7002e56834b30b66fadb23cb8d295eeca9697fbd
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-04-04 16:12:23 -0600

    Update APP0.h

--- trash/APP0.h
+++ trash/APP0.h
@@ -1,1 +1,20 @@
+#ifndef APP0_H
+#define APP0_H
 
+#include <Arduino.h>
+
+/* Paquetes */
+extern byte* DUO_OS[];
+
+/* Texturas */
+extern byte c_DUO_OS_1[8];
+extern byte c_DUO_OS_2[8];
+extern byte c_DUO_OS_3[8];
+extern byte c_DUO_OS_4[8];
+extern byte c_DUO_OS_5[8];
+extern byte c_DUO_OS_6[8];
+extern byte c_DUO_OS_7[8];
+extern byte c_DUO_OS_8[8];
+
+
+#endif
