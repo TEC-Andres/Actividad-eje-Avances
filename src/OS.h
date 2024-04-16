31814f9b75f0ca93881f309a0db04863d585dc5c
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-04-15 23:14:30 -0600

    Update and rename DUO.h to OS.h

--- src/OS.h
+++ src/OS.h
@@ -4,24 +4,26 @@
 #
 #       Copyright (C) Tecnologico de Monterrey
 #
-#       Archivo: DUO.h
+#       Archivo: OS.h
 #
 #       Datos del programa (Sin contar la consola de debugging)
-#       Almacenamiento: 7198 (22%) / 32,256 bytes
-#       RAM (Globales): 612 (29%) / 2,048 bytes
+#       Almacenamiento: 7658 (23%) / 32,256 bytes
+#       RAM (Globales): 734 (35%) / 2,048 bytes
 #
 #       Creado:                   04/04/2024
-#       Última Modificación:      04/04/2024
+#       Última Modificación:      15/04/2024
 */
 
-#ifndef DUO_H
-#define DUO_H
+#ifndef OS_H
+#define OS_H
 
 /* Librerias */
 #include <Arduino.h>
 
 /* Paquetes */
 extern byte* p_DUO_OS[];
+extern byte* p_OS_BARRA[];
+extern byte* p_icons[];
 
 /* Texturas */
 extern byte c_DUO_OS_1[8];
@@ -32,12 +34,19 @@ extern byte c_DUO_OS_5[8];
 extern byte c_DUO_OS_6[8];
 extern byte c_DUO_OS_7[8];
 extern byte c_DUO_OS_8[8];
+extern byte c_OS_BARRA_1[8];
+extern byte c_OS_BARRA_2[8];
+extern byte c_OS_BARRA_3[8];
+extern byte c_OS_BARRA_4[8];
+extern byte c_DUO_ICO[8];
+extern byte c_COOKIE_ICO[8];
+extern byte c_CONFIG_ICO[8];
 
 /* Funciones */
-class DUO_OS {
+class OS_DUO {
 public:
   void LCD_START();
-  void crearCaracter(byte* CARACTER[], uint8_t LIMITE);
+  void crearCaracter(byte* CARACTER[], uint8_t INICIO, uint8_t LIMITE, uint8_t CORRECION);
   void animar(byte* ANIMACION[], int &currentFrame, uint8_t LIMITE, uint8_t MEMORIAVAL);
 };
 
