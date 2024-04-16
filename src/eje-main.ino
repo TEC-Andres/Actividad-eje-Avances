11410b6ab215cb567e9149a3774463aa59bf8b1f
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-04-15 23:14:51 -0600

    Update eje-main.ino

--- src/eje-main.ino
+++ src/eje-main.ino
@@ -7,23 +7,23 @@
 #       Archivo: eje-main.ino
 #
 #       Datos del programa (Sin contar la consola de debugging)
-#       Almacenamiento: 7198 (22%) / 32,256 bytes
-#       RAM (Globales): 612 (29%) / 2,048 bytes
+#       Almacenamiento: 7658 (23%) / 32,256 bytes
+#       RAM (Globales): 734 (35%) / 2,048 bytes
 #
 #       Creado:                   08/01/2024
-#       Última Modificación:      04/04/2024
-*/ 
+#       Última Modificación:      15/04/2024
+*/
 
 /* Librerias de componentes */
 #include <Wire.h>
-#include <OneWire.h>              // [Wokwi]
-#include <DallasTemperature.h>    // [Wokwi]
+#include <OneWire.h>            // [Wokwi]
+#include <DallasTemperature.h>  // [Wokwi]
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 
 /* Librerias de Aplicaciones */
-#include "DUO.h"
-DUO_OS Duo;
+#include "OS.h"
+OS_DUO OS;
 
 /* Definiciones de componentes */
 #define B_VOL A0
@@ -43,18 +43,17 @@ OneWire oneWire(B_TMP);
 DallasTemperature TMP(&oneWire);
 
 /* Declare the LiquidCrystal_I2C object */
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 /* Variables Globales */
-int frame = 1; 
+int frame = 0;
 int POSx;
 int POSy;
 float gradosC;
 double voltage;
 double resistencia;
 
-void setup()
-{
+void setup() {
   lcd.init();
   lcd.backlight();
   pinMode(B_BUZ, INPUT);
@@ -63,31 +62,29 @@ void setup()
   Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
   /* Setup de Librerias */
   TMP.requestTemperatures();
 
   /* Lectura del Hardware */
-  int B_IZQ_R = digitalRead(B_IZQ);         // Lectura del botón Izquierdo
-  int B_ARR_R = digitalRead(B_ARR);         // Lectura del botón Arriba
-  int B_ABJ_R = digitalRead(B_ABJ);         // Lectura del botón Abajo
-  int B_DER_R = digitalRead(B_DER);         // Lectura del botón Derecho
-  int B_SEL_R = digitalRead(B_SEL);         // Lectura del botón Seleccionar
-  int B_SHF_R = digitalRead(B_SHF);         // Lectura del botón Shift
-  int B_VOL_R = analogRead(B_VOL);          // Lectura del potenciometro Volumen
-  float A_PHS_R = analogRead(B_PHS);        // Lectura del sensor de Fotoresistencia
+  int B_IZQ_R = digitalRead(B_IZQ);   // Lectura del botón Izquierdo
+  int B_ARR_R = digitalRead(B_ARR);   // Lectura del botón Arriba
+  int B_ABJ_R = digitalRead(B_ABJ);   // Lectura del botón Abajo
+  int B_DER_R = digitalRead(B_DER);   // Lectura del botón Derecho
+  int B_SEL_R = digitalRead(B_SEL);   // Lectura del botón Seleccionar
+  int B_SHF_R = digitalRead(B_SHF);   // Lectura del botón Shift
+  int B_VOL_R = analogRead(B_VOL);    // Lectura del potenciometro Volumen
+  float A_PHS_R = analogRead(B_PHS);  // Lectura del sensor de Fotoresistencia
   //float B_TMP_R = analogRead(B_TMP);      // [Tinkercad] Lectura del sensor de Temperatura
 
   /* Variables Locales */
   voltage = A_PHS_R / 1024. * 5;
   resistencia = 2000 * voltage / (1 - voltage / 5);
   double B_PHS_R = pow(50 * 1e3 * pow(10, 0.7) / resistencia, (1 / 0.7));
-  gradosC = TMP.getTempCByIndex(0);         // [Wokwi] Lectura del sensor de Temperatura
-  
+  gradosC = TMP.getTempCByIndex(0);  // [Wokwi] Lectura del sensor de Temperatura
+
   /* Código Loop */
-  //tone(B_BUZ, 1500, 15);
-  Duo.LCD_START();
+  OS.LCD_START();
   delay(160);
   /* Debugging */
   //Serial.println("IZQ: " + String(B_IZQ_R) + "\nARR: " + String(B_ARR_R) + "\nABJ: " + String(B_ABJ_R) + "\nDER: " + String(B_DER_R) + "\nSEL: " + String(B_SEL_R) + "\nSHF: " + String(B_SHF_R) + "\nVOL: " + String(B_VOL_R) + "\nTMP: " + String(gradosC) + "C\nPHS: " + String(B_PHS_R) + "\n");
