d9f9210e8973945afea956fee63f766f331e4c0d
TEC-Andres <141695629+TEC-Andres@users.noreply.github.com>
2024-04-22 11:01:17 -0600

    Create CONFIG.h

---
+++ src/CONFIG.h
@@ -0,0 +1,29 @@
+/*
+#       Sesión 30: Actividad eje
+#       Andrés Rodríguez Cantú ─ Daniela Mendez Rivas
+#
+#       Copyright (C) Tecnologico de Monterrey
+#
+#       Archivo: CONFIG.h
+#
+#       Datos del programa (Sin contar la consola de debugging)
+#       Almacenamiento: 15904 (49%) / 32,256 bytes
+#       RAM (Globales): 1409 (68%) / 2,048 bytes
+#
+#       Creado:                   22/04/2024
+#       Última Modificación:      22/04/2024
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Librerias */
+#include <Arduino.h>
+
+/* Funciones */
+class CONFIG_APP {
+public:
+  void START();
+};
+
+#endif
